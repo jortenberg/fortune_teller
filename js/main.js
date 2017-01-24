869c7df5572195519aa9369d22c729971ab49404
jortenberg <jill.ortenberg@gmail.com>
2017-01-24 00:27:31 -0500

    home stretch almost

--- js/main.js
+++ js/main.js
@@ -386,16 +386,21 @@ $(document).ready(function() {
       $("#deterNum").text(num);
       $("#deterIcon").text(iconly);
 
-
       var determiner = $("#determiner"),
-          tl = new TimelineMax();
+          tl = new TimelineMax({onComplete: theFortune});
 
       tl
         .set(determiner, {css: {display: "block", visibility: "hidden"}})
-        .to(wholeThing, .5, {scale: 0.2, y: -290, rotation: 180, ease:Power2.easeInOut})
+        .to(wholeThing, .5, {scale: 0.2, y: "-80%", rotation: 180, ease:Power2.easeInOut})
         .to(inside, .5, {autoAlpha: 0, ease:Power2.easeInOut}, "-=0.5")
-        .fromTo(determiner, 0.5, {autoAlpha:0}, {autoAlpha: 1, ease: Power2.easeInOut});
+        .fromTo(determiner, 0.5, {autoAlpha:0, scale: 0.2, transformOrigin: "50% 50%", y: "-100%"}, {autoAlpha: 1, scale: 1.0, ease: Power2.easeInOut})
+        .to(determiner, 0.5, {autoAlpha:0, scale: 0.2, transformOrigin: "50% 50%", y: "-100%", ease: Power2.easeInOut}, "+=2.0");
+    
+      return tl;
+    }
 
+    function theFortune() {
+      console.log("almost there ya hoo baby");
     }
 
     // 88888888888888888888888888888
@@ -405,7 +410,7 @@ $(document).ready(function() {
       .set(mainSubhead, {autoAlpha: 0})
       .to(h1, .4, {y: -80, delay: 2.5, autoAlpha: 0, ease:Power3.easeIn})
       .to(mainSubhead, .5, {delay: .8, autoAlpha: 1, ease:Power3.easeIn})
-      .to(wholeThing, .5, {autoAlpha: 1, ease:Power3.easeIn}, '-=0.5');
+      .to(wholeThing, .5, {autoAlpha: 1, ease:Power3.easeIn}, "-=0.5");
 
     // masterTl.timeScale(0.5);
 
